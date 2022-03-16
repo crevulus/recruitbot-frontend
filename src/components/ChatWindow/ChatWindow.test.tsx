0a40b340019e81f345df7eb66ff4e282e9480d0e
crevulus <crevulus@gmail.com>
2022-03-16 09:28:00 +0100

    mock fetch calls in tests

--- src/components/ChatWindow/ChatWindow.test.tsx
+++ src/components/ChatWindow/ChatWindow.test.tsx
@@ -1,20 +1,14 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import ChatWindow from ".";
 import App from "../../App";
 import { baseMockContext, customRender } from "../../utils/test-utils";
 
-describe("main chat window logic", () => {
-  it("should show loading before fetchResults are made", () => {
-    const element = document.createElement("div");
-    customRender(<App domElement={element} />, {});
-    const chatWindow = screen.getByTestId(/loading/i);
-    expect(chatWindow).toBeInTheDocument();
-  });
-
+describe("ChatWindow", () => {
   const amendedMockContext = {
     ...baseMockContext,
     fetchResults: {
-      isLoading: false,
+      isLoading: true,
+      error: false,
       data: {
         cta: "this is a trick",
         conversation: [
@@ -27,6 +21,38 @@ describe("main chat window logic", () => {
       },
     },
   };
+
+  describe("ChatWindow: Fetch Logic", () => {
+    let mockFetch: jest.SpyInstance<Promise<Response>>;
+
+    beforeEach(async () => {
+      const mockResponse = amendedMockContext.fetchResults;
+      mockFetch = jest.spyOn(global, "fetch");
+      // @ts-ignore
+      mockFetch.mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockResponse),
+        });
+      });
+    });
+
+    afterEach(() => {
+      mockFetch.mockRestore();
+    });
+
+    it("should make a fetch call", async () => {
+      const element = document.createElement("div");
+      await customRender(<App domElement={element} />, {});
+      await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
+    });
+
+    it("should show loading before fetchResults are made", async () => {
+      const element = document.createElement("div");
+      await customRender(<App domElement={element} />, {});
+      const chatWindow = screen.getByTestId(/loading/i);
+      await waitFor(() => expect(chatWindow).toBeInTheDocument());
+    });
+  });
 
   it("should not show chat if openWidget param not present on load", () => {
     Object.defineProperty(window, "location", {
@@ -41,24 +67,11 @@ describe("main chat window logic", () => {
 
   it("should show chat if openWidget param = true", () => {
     const mockOpenWidgetParam = true;
-    const amendedMockContext = {
-      ...baseMockContext,
+    const localAmendedMockContext = {
+      ...amendedMockContext,
       showChat: mockOpenWidgetParam,
-      fetchResults: {
-        isLoading: false,
-        data: {
-          cta: "this is a trick",
-          conversation: [
-            {
-              id: 1,
-              text: "Thanks for your response! We will get back to you shortly.",
-              answers: [],
-            },
-          ],
-        },
-      },
     };
-    customRender(<ChatWindow />, { contextProps: amendedMockContext });
+    customRender(<ChatWindow />, { contextProps: localAmendedMockContext });
     const chatWindow = screen.getByTestId("chat-window");
     expect(chatWindow).toHaveStyle("max-height: 100vh");
   });
