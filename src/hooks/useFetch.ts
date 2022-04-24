411dc3669efd02e51151868b66fb6ae80a5bb2de
crevulus <crevulus@gmail.com>
2022-04-24 16:15:39 +0200

    edit db data

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -11,9 +11,10 @@ const ERROR_MESSAGE_COPY =
   "Oops! Something went wrong. Hit the refresh button to try that again.";
 
 export const ROOT_API_URL =
-  process.env.NODE_ENV === Environments.Prod
-    ? process.env.REACT_APP_PROD_URL
-    : process.env.REACT_APP_DEV_URL;
+  // process.env.NODE_ENV === Environments.Prod
+  //   ? process.env.REACT_APP_PROD_URL
+  //   : process.env.REACT_APP_DEV_URL;
+  process.env.REACT_APP_PROD_URL;
 
 export const useFetch = <T>({
   url,
